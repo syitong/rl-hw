7f1d6baed2f7a852fd479ad8bb9b6c375946f921
Yitong Sun <syitong@umich.edu>
2018-10-12 16:36:46 -0400

    ready to debug dynaq

--- hw3/dynaq.py
+++ hw3/dynaq.py
@@ -43,12 +43,21 @@ def testshow(env,policy):
 def _onestep_q(s, a, ss, r, Q, gamma=1, alpha=0.9):
     Q[s,a] = Q[s,a] + alpha * (r + gamma * np.max(Q[ss]) - Q[s,a])
 
+def model(P,s,a):
+    p , keys = [], []
+    tot = P[(s,a)][0]
+    for key,val in P[(s,a)][1].items():
+        p.append(val / tot)
+        keys.append(key)
+    return np.random.choice(keys,p=p)
+
 def dynaq(env,n=10,gamma=1,alpha=0.9,ep=0.05,episodes=1000):
     nS = env.nS
     nA = env.nA
     rew_alloc = []
-    P = {} # {s:{a:(tot_num_visit,[{ss:num_visit},],exp_rew),},}
+    P = {} # {(s,a):(tot_num_visit,{ss:num_visit},exp_rew)}
     Q = np.zeros((nS,nA))
+    tot_steps = 0
     rew_list = np.zeros(episodes)
     for idx in range(episodes):
         s = env.reset()
@@ -57,13 +66,26 @@ def dynaq(env,n=10,gamma=1,alpha=0.9,ep=0.05,episodes=1000):
         cum_rew = 0
         while not done:
             a = ep_greedy(Q,s,ep)
-            s, r, done, _ = env.step(a)
-            s_onestep_q(s, a, ss, r, Q, gamma, alpha)
+            ss, r, done, _ = env.step(a)
+            _onestep_q(s, a, ss, r, Q, gamma, alpha)
+            # update the model
+            P.setdefault((s,a),(0,{},0))
+            P[(s,a)][0] += 1
+            P[(s,a)][1][ss] = P[(s,a)][1].setdefault(ss,0) + 1
+            P[(s,a)][2] = (P[(s,a)][2] * (P[(s,a)][0] - 1) + r) / P[(s,a)][0]
+            cum_rew +=  gamma**counter * r
             s = ss
-            cum_rew = cum_rew * gamma + r
             counter += 1
+            tot_steps += 1
+            pairs = list(P.keys())
+            # planning step
+            for jdx in range(n):
+                s,a = pairs(np.random.choice(len(pairs)))
+                ss = model(P,s,a)
+                r = P[(s,a)][2]
+                _onestep_q(s,a,ss,r,Q)
         print(counter,idx)
         rew_list[idx] = cum_rew
     rew_alloc.append(rew_list)
     rew_list = np.mean(np.array(rew_alloc),axis=0)
-    return Q, rew_list
+    return Q, rew_list, tot_steps
