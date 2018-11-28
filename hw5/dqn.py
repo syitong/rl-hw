eacb574deebaeb3c85a8f5f5aa59c3f7bd3aaef6
Yitong Sun <syitong@umich.edu>
2018-11-28 03:02:11 +0000

    add in original dqn tricks

--- hw5/dqn.py
+++ hw5/dqn.py
@@ -26,15 +26,15 @@ class memory(list):
             output += [np.random.choice(self)]
         return output
 
-def Qtable(Q, a_list):
-    def proxy(s):
-        Qtable = []
-        for a in a_list:
-            Qtable += [Q(s,a)]
-        return Qtable
-    return proxy
+# def Qtable(Q, a_list):
+#     def proxy(s):
+#         Qtable = []
+#         for a in a_list:
+#             Qtable += [Q(s,a)]
+#         return Qtable
+#     return proxy
 
-def dqn(N, num_episodes, env, ep, batch_size, gamma, a_list, C, lrate, T=5000):
+def dqn(N, num_episodes, env, ep_decay_rate, batch_size, gamma, a_list, C, lrate, T=5000):
     nA = len(a_list)
     D = memory(N)
     dS = 2
@@ -42,10 +42,12 @@ def dqn(N, num_episodes, env, ep, batch_size, gamma, a_list, C, lrate, T=5000):
     Q = model.Q
     Qhat = model.Qhat
     num_steps = []
+    iter = 0
     for episode in range(num_episodes):
         s = env.reset()
+        ep = (1-0) / (episode * ep_decay_rate + 1)
         for t in range(T):
-            a = ep_greedy(Qtable(Q, [0,1,2]), s, ep)
+            a = ep_greedy(Q, s, ep)
             ss, r, done, _ = env.step(a)
             D.add({'s':s,'a':a,'r':r,'ss':ss,'done':done})
             batch = D.sample(batch_size)
@@ -57,18 +59,21 @@ def dqn(N, num_episodes, env, ep, batch_size, gamma, a_list, C, lrate, T=5000):
                     y[idx] = batch[idx]['r']
                 else:
                     y[idx] = batch[idx]['r'] + \
-                        gamma * max([Qhat(batch[idx]['ss'], a) for a in a_list])
+                        gamma * max(Qhat(batch[idx]['ss']))
+                # y[idx] = min(-5,y[idx])
                 s_batch += [list(batch[idx]['s'])]
                 a_batch += [batch[idx]['a']]
             model.fit(np.array(s_batch), np.array(a_batch), y, lrate)
-            if t % C == 0:
+            if iter % 100 == 0:
+                loss = model.get_loss(np.array(s_batch), np.array(a_batch), y)
+            if t % C == C - 1:
                 model.update()
+            iter += 1
             if done:
-                steps = t
                 break
-            print('\repisode: {}, # of steps: {}'.format(episode, t),end='')
+            print('\repisode: {}, # of steps: {:<8}, loss: {:<.3}'.format(episode, t, loss),end='')
             sys.stdout.flush()
-        num_steps += [steps]
+        num_steps += [t]
     return num_steps
 
 def plot_dqn(num_steps):
@@ -81,21 +86,21 @@ def plot_dqn(num_steps):
     plt.close()
 
 if __name__ == '__main__':
-    N = 50
-    num_episodes=50
+    N = 500
+    num_episodes=1000
     register(
         id='MountainCar-v1',
         entry_point='gym.envs.classic_control:MountainCarEnv',
-        max_episode_steps=2000,
+        max_episode_steps=5000,
         reward_threshold=-110.0,
     )
     env = gym.make('MountainCar-v1')
-    ep = 0.1
-    batch_size = 5
+    ep_decay_rate = 0.5
+    batch_size = 50
     gamma = 1
     a_list = [0,1,2]
-    C = 10
-    lrate = 0.1
+    C = 1
+    lrate = 0.2
 
     num_steps = dqn(N, num_episodes, env, ep, batch_size, gamma, a_list, C, lrate)
     plot_dqn(num_steps)
--- hw5/model.py
+++ hw5/model.py
@@ -7,9 +7,21 @@ class nn_model:
         self.a_list = a_list
         self.w = {}
         self.w_t = {}
+        self.assign_op = {}
         self._graph = tf.Graph()
         self._sess = tf.Session(graph=self._graph)
         self._build()
+
+    def get_loss(self, states, actions, targets):
+        if hasattr(self, 'loss'):
+            feed_dict = {
+                'states:0':states,
+                'actions:0':actions,
+                'targets:0':targets,
+            }
+            return self._sess.run(self.loss, feed_dict=feed_dict)
+        else:
+            print('loss has not been calculated.')
 
     def __del__(self):
         self._sess.close()
@@ -22,54 +34,61 @@ class nn_model:
             y = tf.placeholder(dtype=tf.float32, shape=[None], name='targets')
             lrate = tf.placeholder(dtype=tf.float32, shape=[], name='lrate')
             onehot = tf.one_hot(a, depth=len(self.a_list))
-            n_features = self.dim + len(self.a_list)
-            x = tf.concat([s, onehot],1)
+            output_dim = len(self.a_list)
             with tf.variable_scope('train'):
                 self.w['l1w'] = l1w = tf.Variable(
-                    tf.truncated_normal(shape=[n_features,10]))
+                    tf.glorot_uniform_initializer()((self.dim,100)))
                 self.w['l1b'] = l1b = tf.Variable(
-                    tf.truncated_normal(shape=[10]))
-                L1 = tf.nn.relu(tf.matmul(x, l1w) + l1b)
-                self.w['l2w'] = l2w = tf.Variable(
-                    tf.truncated_normal(shape=[10,10]))
-                self.w['l2b'] = l2b = tf.Variable(
-                    tf.truncated_normal(shape=[10]))
-                L2 = tf.nn.relu(tf.matmul(L1, l2w) + l2b)
-                self.w['l3w'] = l3w = tf.Variable(
-                    tf.truncated_normal(shape=[10,10]))
-                self.w['l3b'] = l3b = tf.Variable(
-                    tf.truncated_normal(shape=[10]))
-                L3 = tf.nn.relu(tf.matmul(L2, l3w) + l3b)
+                    tf.constant(0., shape=[100]))
+                L1 = tf.nn.relu(tf.matmul(s, l1w) + l1b)
+                # self.w['l2w'] = l2w = tf.Variable(
+                #     tf.glorot_uniform_initializer()((10,10)))
+                # self.w['l2b'] = l2b = tf.Variable(
+                #     tf.constant(0., shape=[10]))
+                # L2 = tf.nn.relu(tf.matmul(L1, l2w) + l2b)
+                # self.w['l3w'] = l3w = tf.Variable(
+                #     tf.glorot_uniform_initializer()((10,10)))
+                # self.w['l3b'] = l3b = tf.Variable(
+                #     tf.constant(0., shape=[10]))
+                # L3 = tf.nn.relu(tf.matmul(L2, l3w) + l3b)
                 self.w['l4w'] = l4w = tf.Variable(
-                    tf.truncated_normal(shape=[10,1]))
-                self.pred = tf.matmul(L3,l4w)
-                self.loss = tf.reduce_mean((self.pred - tf.reshape(y,[-1,1]))**2)
+                    tf.glorot_uniform_initializer()((100,output_dim)))
+                self.pred = tf.matmul(L1,l4w)
+                q_act = tf.reduce_mean(self.pred * onehot, reduction_indices=1)
+                self.loss = tf.reduce_mean((q_act  - tf.reshape(y,[-1,1]))**2)
             with tf.variable_scope('target'):
                 self.w_t['l1w'] = l1w = tf.Variable(
-                    tf.constant(0., shape=[n_features,10]))
+                    tf.constant(0., shape=[self.dim,100]))
                 self.w_t['l1b'] = l1b = tf.Variable(
-                    tf.constant(0., shape=[10]))
-                L1 = tf.nn.relu(tf.matmul(x, l1w) + l1b)
-                self.w_t['l2w'] = l2w = tf.Variable(
-                    tf.constant(0., shape=[10,10]))
-                self.w_t['l2b'] = l2b = tf.Variable(
-                    tf.constant(0., shape=[10]))
-                L2 = tf.nn.relu(tf.matmul(L1, l2w) + l2b)
-                self.w_t['l3w'] = l3w = tf.Variable(
-                    tf.constant(0., shape=[10,10]))
-                self.w_t['l3b'] = l3b = tf.Variable(
-                    tf.constant(0., shape=[10]))
-                L3 = tf.nn.relu(tf.matmul(L2, l3w) + l3b)
+                    tf.constant(0., shape=[100]))
+                L1 = tf.nn.relu(tf.matmul(s, l1w) + l1b)
+                # self.w_t['l2w'] = l2w = tf.Variable(
+                #     tf.constant(0., shape=[10,10]))
+                # self.w_t['l2b'] = l2b = tf.Variable(
+                #     tf.constant(0., shape=[10]))
+                # L2 = tf.nn.relu(tf.matmul(L1, l2w) + l2b)
+                # self.w_t['l3w'] = l3w = tf.Variable(
+                #     tf.constant(0., shape=[10,10]))
+                # self.w_t['l3b'] = l3b = tf.Variable(
+                #     tf.constant(0., shape=[10]))
+                # L3 = tf.nn.relu(tf.matmul(L2, l3w) + l3b)
                 self.w_t['l4w'] = l4w = tf.Variable(
-                    tf.constant(0., shape=[10,1]))
-                self.pred_t = tf.matmul(L3, l4w)
+                    tf.constant(0., shape=[100,output_dim]))
+                self.pred_t = tf.matmul(L1, l4w)
             with tf.variable_scope('optimize'):
-                optimizer = tf.train.AdamOptimizer(learning_rate=
-                    lrate)
+                # optimizer = tf.train.AdamOptimizer(learning_rate=
+                #     lrate)
+                optimizer = tf.train.GradientDescentOptimizer(
+                    learning_rate=lrate
+                )
                 self.train_op = optimizer.minimize(loss=self.loss,
                     global_step=global_step)
                 init_op = tf.global_variables_initializer()
+            with tf.variable_scope('assign'):
+                for key in self.w.keys():
+                    self.assign_op[key] = tf.assign(self.w_t[key], self.w[key])
             self._sess.run(init_op)
+        self._graph.finalize()
 
     def fit(self, states, actions, targets, lrate):
         with self._graph.as_default():
@@ -83,21 +102,18 @@ class nn_model:
 
     def update(self):
         for key in self.w.keys():
-            self.w_t[key].assign(self.w[key])
+            self._sess.run(self.assign_op[key])
 
     # evaluate the trained network
-    def Q(self, state, action):
+    def Q(self, state):
         feed_dict = {
             'states:0': np.array(state).reshape((1,-1)),
-            'actions:0': [action]
         }
-
-        return self._sess.run(self.pred, feed_dict)
+        return self._sess.run(self.pred, feed_dict)[0]
 
     # evaluate the target network
-    def Qhat(self, state, action):
+    def Qhat(self, state):
         feed_dict = {
             'states:0': np.array(state).reshape((1,-1)),
-            'actions:0': [action]
         }
-        return self._sess.run(self.pred, feed_dict)
+        return self._sess.run(self.pred_t, feed_dict)[0]
