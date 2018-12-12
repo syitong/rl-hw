cf4a9ecbab57aab974a20f4dc6cc7b3e6e4c68de
Sun, Yitong <sunny19890224@gmail.com>
2018-12-11 22:51:54 -0500

    rewrite the code according to the homework

--- hw5/dqn.py
+++ hw5/dqn.py
@@ -24,34 +24,30 @@ class memory(list):
         output = np.random.choice(self,size)
         return output
 
-def dqn(N, max_iter, env,
-        ep_start, batch_size,
-        gamma, a_list, C, lrate, lambda_, test_rounds=10,
-        learning_starts=32, T=200, epsilon_rate=0.1):
+def dqn(N, env, ep_start, ep_end, ep_rate, batch_size,
+        gamma, a_list, C, lrate, lambda_, criteria, test_episodes=100):
     nA = len(a_list)
     D = memory(N)
-    nS = 2
+    nS = env.observation_space.shape[0]
     model = nn_model(nS, a_list, 'test1', lrate) # implement two networks in one model with an update method.
-    model.update()
     Q = model.Q
     Qhat = model.Qhat
-    num_steps = []
-    success = 0
+    score_list = []
     iter = 0
-    while iter < max_iter:
+    episode = learn_starts = 0
+    while True:
         s = env.reset()
         done = False
-        total_r = 0
-        t = 0
-        if iter > learning_starts:
-            ep = ep_start - min((ep_start - 0.1) /
-                (0.1 * (max_iter - learning_starts)) * (iter - learning_starts), ep_start - 0.1)
-        else:
-            ep = 1.
+        ep = ep_start - min(ep_rate * (episode - learn_starts), ep_start - ep_end)
         while not done:
             a = ep_greedy(Q, s, ep)
             ss, r, done, _ = env.step(a)
             D.add({'s':s,'a':a,'r':r,'ss':ss,'done':done})
+            s = ss
+            if iter < batch_size:
+                iter += 1
+                learn_starts = episode
+                continue
             batch = D.sample(batch_size)
             y = np.empty(batch_size)
             s_batch = []
@@ -64,84 +60,70 @@ def dqn(N, max_iter, env,
                         gamma * max(Qhat(batch[idx]['ss']))
                 s_batch += [batch[idx]['s']]
                 a_batch += [batch[idx]['a']]
-            if iter > learning_starts:
-                model.fit(np.array(s_batch), np.array(a_batch), y)
-            s = ss.copy()
+            model.fit(np.array(s_batch), np.array(a_batch), y)
             if iter % 100 == 0:
                 loss = model.get_loss(np.array(s_batch), np.array(a_batch), y)
-            if iter > learning_starts and iter % C == C - 1:
+                print('\rtotal iter: {}, episode: {}, loss: {:<.4}     '.format(iter, episode, loss),end='')
+                sys.stdout.flush()
+            if iter % C == C - 1:
                 model.update()
-            total_r += r
-            t += 1
-            print('\rtotal iter: {}, # of steps: {:<5}, loss: {:<.4}, # of success: {}     '.format(
-                iter, t, loss, success),end='')
-            sys.stdout.flush()
-            if iter % 1000 == 0:
-                print('')
-                num_steps += [eval_perform(model, env, test_rounds)]
-                env.reset()
             iter += 1
-            if done:
+        if episode % 100 == 0:
+            print('')
+            score = eval_perform(model, env, test_episodes)
+            score_list += [score]
+            if score > criteria:
                 break
-        if total_r > -190:
-            success += 1
-        if success > 100:
-            break
+        episode += 1
     model.save()
-    return num_steps, model
+    return score_list, model
 
-def eval_perform(agent, env, rounds):
+def eval_perform(agent, env, episodes):
     avg_score = 0
-    for idx in range(rounds):
+    for idx in range(episodes):
         score = 0
         done = False
         s = env.reset()
         while not done:
-            env.render()
+            # env.render()
             a = ep_greedy(agent.Q, s, 0.)
             ss, r, done, _ = env.step(a)
             score += r
-            s = ss.copy()
-            print('\r', score ,a, '            ', end='')
-        sys.stdout.flush()
+            s = ss
         avg_score = (avg_score * idx + score) / (idx + 1)
-    print('')
+    print('avg_score: {}'.format(avg_score))
     return avg_score
 
 def plot_dqn(num_steps, name):
     fig = plt.figure()
     plt.plot(np.array(num_steps))
     plt.title('Performance of DQN on Mountain Car')
-    plt.xlabel('per 50 episodes')
-    plt.ylabel('avg # of steps')
+    plt.xlabel('per 100 episodes')
+    plt.ylabel('score in testing')
     plt.savefig(name+'.eps')
     plt.close()
 
 if __name__ == '__main__':
-    prefix = sys.argv[1]
-    # np.random.seed(3)
-    N = 50000
-    max_iter = 100000
-    register(
-        id='MountainCar-v1',
-        entry_point='gym.envs.classic_control:MountainCarEnv',
-        max_episode_steps=20000,
-        reward_threshold=-110.0,
-    )
-    # env = gym.make('CartPole-v0')
-    env = gym.make('MountainCar-v0')
-    ep_start = 1.
+    np.random.seed(3)
+    name = 'CartPole-v0'
+    env = gym.make(name)
+    N = 10000
+    # env = gym.make('MountainCar-v0')
+    criteria = 195
+    ep_start = 0.5
+    ep_end = 0.01
+    ep_rate = 0.0001
     batch_size = 32
-    gamma = 0.9
-    a_list = [0,1,2]
-    C = 500
-    lrate = 0.001
+    gamma = 1.
+    a_list = [0,1]
+    C = 1
+    lrate = 0.0001
     lambda_ = 0.
 
     t1 = time.process_time()
-    num_steps, agent = dqn(N, max_iter, env,
-        ep_start, batch_size, gamma, a_list, C, lrate, lambda_)
+    score_list, agent = dqn(N, env,
+        ep_start, ep_end, ep_rate, batch_size, gamma, a_list, C, lrate, lambda_, criteria)
     t2 = time.process_time()
     print('training time:', t2-t1)
-    plot_dqn(num_steps, 'test-'+str(prefix))
+    plot_dqn(score_list, name)
     # agent = nn_model(2, a_list, 'test1', lambda_, lrate, load=True) # implement two networks in one model with an update method.
